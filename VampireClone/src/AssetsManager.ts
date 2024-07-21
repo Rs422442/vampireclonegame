4bf1ae6c0925574cd4fd8eac4d988eb73facda3d
Dmitrii Chesnokov <pommidorka2@gmail.com>
2024-07-21 11:29:30 +0200

    fix: resource loading fix
    Была ошибка в названии загружаемых файлов.
    Исправил нейминги + добавил вывод ошибки при попытке загрузить файл по неправильной ссылке
    Бонусом потенциальные настройки для спрайта.

--- VampireClone/src/AssetsManager.ts
+++ VampireClone/src/AssetsManager.ts
@@ -1,71 +1,76 @@
-import * as UTILS from '@pixi/utils';
-import * as PIXI from 'pixi.js';
+import * as UTILS from "@pixi/utils";
+import * as PIXI from "pixi.js";
 
-type atlassheet = [name: string, path: string]; 
+type atlassheet = [name: string, path: string];
 
-export default class AssetManager{
-    resources: any;
-    assetsloader= PIXI.Loader.shared;
-    promises: Promise<void>[] = [];
-    Element = document.getElementById('preloader')
+export default class AssetManager {
+	resources: any;
+	assetsloader = PIXI.Loader.shared;
+	promises: Promise<void>[] = [];
+	Element = document.getElementById("preloader");
 
-    constructor()
-    {   
+	constructor() {}
 
-    };
+	async preload(atlas: atlassheet[]): Promise<void> {
+		this.promises.push(
+			new Promise<void>((resolve, _reject) => {
+				atlas.forEach((value) => {
+					this.assetsloader.add(value[0], value[1]);
+					console.warn("sprite add " + value[0] + " " + value[1]);
+				});
 
-    async preload(atlas: atlassheet[]): Promise<void> {            
-        this.promises.push(
-          new Promise<void>((resolve, _reject) =>
-            {              
-              atlas.forEach((value) =>
-              {              
-                this.assetsloader.add(value[0], value[1]);               
-                console.warn('sprite add '+ value[0] + ' ' + value[1]);
-              }); 
+				this.assetsloader.load((_assetsloader, _resources) => {
+					this.resources = _resources;
+					// Добавил вот тут вывод в консоль для всех проблемных ресурсов.
+					// Внимание! Сейчас я обрабатываю ЛЮБУЮ ошибку как ошибку ненахождения ресурса.
+					// вероятно, правильнее было бы смотреть на тип ошибки и выводить соответствующее сообщение.
+					for (let key in _resources) {
+						if (_resources[key].error) {
+							console.error(
+								`${_resources[key].name}\nnot find by url: ${_resources[key].url}`
+							);
+						}
+					}
+					console.warn("all sprites loaded");
+					console.warn(_resources);
+					resolve();
+				});
+			})
+		);
 
-              this.assetsloader.load((_assetsloader, _resources) =>          
-              {
-                this.resources = _resources;
-                console.warn('all sprites loaded');
-                console.warn(_resources);
-                resolve();
-              });
-            })
-        ); 
+		await Promise.all(this.promises)
+			.then((_result) => {
+				this.Element?.classList.add("preloader--hide");
+				console.warn("preloader hide");
+			})
+			.catch((_err) => console.warn("error loading resources"));
+	}
 
-        await Promise.all(this.promises)
-          .then((_result) => {this.Element?.classList.add('preloader--hide'); console.warn('preloader hide')})
-          .catch((_err) => console.warn('error loading resources'))
-      }
+	private getTextureFromResources(
+		resources: UTILS.Dict<PIXI.Texture<PIXI.Resource>>,
+		key: string,
+		frame?: string
+	): PIXI.Texture {
+		if (frame) {
+			if (!(resources[key] as any).data.frames[frame].frame) {
+				console.error(`[getTexture]: В ${key} нет ${frame}`);
+			}
+			return (resources[key] as any).data.frames[frame].frame.texture;
+		}
 
-    private getTextureFromResources(resources: UTILS.Dict<PIXI.Texture<PIXI.Resource>>, key: string, frame?: string): PIXI.Texture
-      {
-        if (frame) 
-          {
-            if (!(resources[key] as any).data.frames[frame].frame)
-              {
-                console.error(`[getTexture]: В ${key} нет ${frame}`);
-              }
-            return (resources[key] as any).data.frames[frame].frame.texture;
-          }
-  
-        if (!key)
-          {
-            console.error("Не указано имя атласа для текстуры");
-            return PIXI.Texture.EMPTY;
-          }
-  
-        if (!(resources[key] as any))
-          {
-            console.error(`[getTexture]: Нет ${key}`);
-          }
-  
-        return (resources[key] as any).texture;
-      }
-  
-    getTexture(key: string, frame?: string): PIXI.Texture
-      {
-        return this.getTextureFromResources(this.resources, key, frame);
-      }
-};
+		if (!key) {
+			console.error("Не указано имя атласа для текстуры");
+			return PIXI.Texture.EMPTY;
+		}
+
+		if (!(resources[key] as any)) {
+			console.error(`[getTexture]: Нет ${key}`);
+		}
+
+		return (resources[key] as any).texture;
+	}
+
+	getTexture(key: string, frame?: string): PIXI.Texture {
+		return this.getTextureFromResources(this.resources, key, frame);
+	}
+}
